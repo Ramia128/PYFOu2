9510efffa8f1b88ff9aa59d4554f8262fd001838
Rami Aldelemi <rammi.aldelemi91@gmail.com>
2024-02-09 14:38:12 +0100

    Fixade skit

--- csv_data.py
+++ csv_data.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 class gdp_data():
     def total():
-        ## Läser csv filen och filtrerar till country name, country code och 1960 - 2022
-        df = pd.read_csv('GDP_TOTAL.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(1960, 2023)])
+        ## Läser csv filen och filtrerar till country name, country code och 2010 - 2022
+        df = pd.read_csv('GDP_TOTAL.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(2010, 2023)])
 
         ## Tar bort decimalen så det blir enklare att läsa
         pd.options.display.float_format = '{:,.0f}'.format
 
-        ## Fyller NaN values till 0
-        df = df.fillna(0)
+        ##  Filtrerar bort NaN
+        df = df.dropna()
         
         ## Sorterar den maximala GDP utifrån år 2022
         df_sorted = df.sort_values(by='2022', ascending=False)
@@ -18,14 +18,14 @@ class gdp_data():
         return df_sorted
     
     def per_capita():
-        ## Läser csv filen och filtrerar till country name, country code och 1960 - 2022
-        df = pd.read_csv('GDP_PCAP.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(1960, 2023)])
+        ## Läser csv filen och filtrerar till country name, country code och 2010 - 2022
+        df = pd.read_csv('GDP_PCAP.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(2010, 2023)])
 
         ## Tar bort decimalen så det blir enklare att läsa
         pd.options.display.float_format = '{:,.0f}'.format
 
-        ## Fyller NaN values till 0
-        df = df.fillna(0)
+        ##  Filtrerar bort NaN
+        df = df.dropna()
 
         ## Sorterar den maximala GDP per capita utifrån år 2022
         df_sorted = df.sort_values(by='2022', ascending=False)
@@ -33,14 +33,14 @@ class gdp_data():
         return df_sorted
     
     def life_expectancy():
-        ## Läser csv filen och filtrerar till country name, country code och 1960 - 2022
-        df = pd.read_csv('Life_Expectancy.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(1960, 2022)])
+        ## Läser csv filen och filtrerar till country name, country code och 2010 - 2022
+        df = pd.read_csv('Life_Expectancy.csv', skiprows=3, usecols=['Country Name'] + [str(x) for x in range(2010, 2022)])
 
         ## Tar bort decimalen så det blir enklare att läsa
         pd.options.display.float_format = '{:,.0f}'.format
 
-        ## Fyller NaN values till 0
-        df = df.fillna(0)
+        ##  Filtrerar bort NaN
+        df = df.dropna()
 
         ## Sorterar livslängden enligt tabellens data
         df_sorted = df.sort_values(by='2021', ascending=False)
@@ -48,32 +48,31 @@ class gdp_data():
         return df_sorted
 
 class gdp_graph():
-    def capita_graph(df, countries, title):
-        fig, ax = plt.subplots(figsize=(10, 5))       
-        df_countries = df[df['Country Name'].isin(countries)]
-        df_plot = df_countries.set_index('Country Name').transpose()
-        df_plot = df_plot.apply(pd.to_numeric, errors='coerce')
+    def life_capita_graph(df_cap, df_life, countries, title_cap, title_life):
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 7))       
+        df_cap_countries = df_cap[df_cap['Country Name'].isin(countries)]
+        df_cap_plot = df_cap_countries.set_index('Country Name').transpose()
+        df_cap_plot = df_cap_plot.apply(pd.to_numeric, errors='coerce')
 
-        df_plot.plot(style='-', ax=ax)
+        df_cap_plot.plot(style='-', ax=ax1)
 
-        plt.title(title)
-        plt.xticks(range(0, len(df_plot.index), 6), df_plot.index[::6])
-        plt.legend()
-        ax.yaxis.set_major_formatter(lambda x, pos: f'{x/1e3:.0f}K')
-        plt.tight_layout()
-        plt.show()
+        ax1.set_title(title_cap)
+        ax1.set_xticks(range(0, len(df_cap_plot.index), 1), df_cap_plot.index[::1])
+        ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        ax1.yaxis.set_major_formatter(lambda x, pos: f'{x/1e3:.0f}K')
+        ax1.set_ylabel('Dollars')
 
-    def expectancy_graph(df, countries, title):
-        fig, ax = plt.subplots(figsize=(10, 5))       
-        df_countries = df[df['Country Name'].isin(countries)]
-        df_plot = df_countries.set_index('Country Name').transpose()
-        df_plot = df_plot.apply(pd.to_numeric, errors='coerce')
+        df_life_countries = df_life[df_life['Country Name'].isin(countries)]
+        df_life_plot = df_life_countries.set_index('Country Name').transpose()
+        df_life_plot = df_life_plot.apply(pd.to_numeric, errors='coerce')
 
-        df_plot.plot(style='-', ax=ax)
+        df_life_plot.plot(style='-', ax=ax2)
 
-        plt.title(title)
-        plt.xticks(range(0, len(df_plot.index), 10), df_plot.index[::10], rotation=45)
-        plt.legend()
-        ax.yaxis.set_major_formatter(lambda x, pos: f'{x/1:.0f}')
+        ax2.set_title(title_life)
+        ax2.set_xticks(range(0, len(df_life_plot.index), 1), df_life_plot.index[::1])
+        ax2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        ax2.yaxis.set_major_formatter('{:.0f}'.format)
+        ax2.set_ylabel('Years')
+
         plt.tight_layout()
         plt.show()
--- main.py
+++ main.py
@@ -7,10 +7,9 @@ while True:
     print('1. Total GDP data')
     print('2. GDP per capita data')
     print('3. Life expendancy data')
-    print('4. GDP per capita graph')
-    print('5. Life expectancy graph')
-    print('6. Exit the program.')
-    choose = input('Enter number between (1-6) ')
+    print('4. GDP per capita and Life expectancy graph')
+    print('5. Exit the program.')
+    choose = input('Enter number between (1-5) ')
 
     if choose == '1':
         print(gdp_data.total())
@@ -20,15 +19,12 @@ while True:
         print(gdp_data.life_expectancy())
     elif choose == '4':
         selected_countries = ['United States', 'Norway', 'Sweden', 'Japan', 'Germany']
-        title = 'GDP per capita'
-        df_total = gdp_data.per_capita()
-        gdp_graph.capita_graph(df_total, selected_countries, title)
+        title_cap = 'GDP per capita'
+        title_life = 'Life Expectancy'
+        df_cap = gdp_data.per_capita()
+        df_life = gdp_data.life_expectancy()
+        gdp_graph.life_capita_graph(df_cap, df_life, selected_countries, title_cap, title_life)
     elif choose == '5':
-        selected_countries = ['United States', 'Norway', 'Sweden', 'Japan', 'Germany']
-        title = 'Life Expectancy'
-        df_total = gdp_data.life_expectancy()
-        gdp_graph.expectancy_graph(df_total, selected_countries, title)
-    elif choose == '6':
         print('Shutting down program.')
         break
     else:
